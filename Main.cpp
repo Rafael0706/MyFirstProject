e2e434b3c2572a375ff8d65978d7cdcf90c71013
Rafael0706 <rafaelgallegos0706@gmail.com>
2024-08-30 00:02:30 -0500

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+/*
+Rafael Gallegos Cespedes
+Computer Science Fall 2024
+Due: Sep. 3, 2024
+Lab 1, Lab 1 Introducction to C++ Programming
+This lab is a basic demo of the C++ language.
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Red" << std::endl;
+
+	return 0;
+}
